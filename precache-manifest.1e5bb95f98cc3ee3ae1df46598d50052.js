4fdef53aa206aa74f117139875d8b8929dc39e80
Dynara <dynara.oliveira@gmail.com>
2019-06-11 21:39:36 -0300

    Updates

---
+++ precache-manifest.1e5bb95f98cc3ee3ae1df46598d50052.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7034da1b35eb94eb95de38d97a61e425",
+    "url": "/fiap_react_master/index.html"
+  },
+  {
+    "revision": "d62f212b44d421086026",
+    "url": "/fiap_react_master/static/css/main.6a354fe3.chunk.css"
+  },
+  {
+    "revision": "a7526d0761dcfd0dca67",
+    "url": "/fiap_react_master/static/js/2.34d4a37c.chunk.js"
+  },
+  {
+    "revision": "d62f212b44d421086026",
+    "url": "/fiap_react_master/static/js/main.cbcc0c3c.chunk.js"
+  },
+  {
+    "revision": "627d1590504baa12ebcf",
+    "url": "/fiap_react_master/static/js/runtime~main.858a751c.js"
+  }
+]);
